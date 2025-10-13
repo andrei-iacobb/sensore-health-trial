a5a6b19ea73bce091b1dd1789d10fef2a644e073
andrei-iacobb <andreiiacob2006@gmail.com>
2025-10-13 17:50:01 +0100

    here i did a bit of work on auth and css, such as adding the sensore logo and working on it all bledning in together

--- sensore-health-trial-1/obj/Debug/net9.0/sensore-health-trial-1.AssemblyInfo.cs
+++ sensore-health-trial-1/obj/Debug/net9.0/sensore-health-trial-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sensore-health-trial-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01ff48db0be19c1c7a02efca6e8cae3170b7ea74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+912004d983fe2d57893845a04394aad6a833927b")]
 [assembly: System.Reflection.AssemblyProductAttribute("sensore-health-trial-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sensore-health-trial-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
